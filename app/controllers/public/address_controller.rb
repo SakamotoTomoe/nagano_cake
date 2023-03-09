c60270a5e3744f2a2c26fd6bd0afe61ac1da3cb3
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-03-09 07:01:21 +0000

    [add]address/edit

--- app/controllers/public/address_controller.rb
+++ app/controllers/public/address_controller.rb
@@ -6,11 +6,20 @@ class Public::AddressController < ApplicationController
 
   def create
     address = Address.new(address_params)
+    address.customer_id = current_customer.id
     address.save
     redirect_to address_index_path
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    address = Address.find(params[:id])
+    address.customer_id = current_customer.id
+    address.update(address_params)
+    redirect_to address_index_path
   end
 
   private
