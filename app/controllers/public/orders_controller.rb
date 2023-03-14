c966627333ad0e543508ba19cfc9a8562040d084
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-03-14 13:21:44 +0000

    [add]order/confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,8 +10,16 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
+    @order = Order.new(orders_params)
+    @order.shipping_cost = 800
     @cart_items = current_customer.cart_items
     @total = 0
     @total_payment = @order.shipping_cost + (@total + cart_item.subtotal)
+  end
+  
+  private
+  
+  def orders_params
+    params.require(:order).permit(:postal_code, :address, :name, :total_payment, :payment_method, :status, :customer_id)
   end
 end
