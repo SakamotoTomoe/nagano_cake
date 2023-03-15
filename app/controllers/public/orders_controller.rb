02ca6b016fc2276e29f2b22d0405db4bf37900b1
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-03-15 12:43:56 +0000

    [add]order/confirm/create

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,17 +8,37 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-  
+
   def confirm
     @order = Order.new(orders_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+      @order.postal_code
+      @order.address
+      @order.name
+    end
+
     @order.shipping_cost = 800
     @cart_items = current_customer.cart_items
     @total = 0
-    @total_payment = @order.shipping_cost + (@total + cart_item.subtotal)
   end
-  
+
+  def create
+    order = Order.new(orders_params)
+    order.save
+    redirect_to orders_complete_path
+  end
+
   private
-  
+
   def orders_params
     params.require(:order).permit(:postal_code, :address, :name, :total_payment, :payment_method, :status, :customer_id)
   end
