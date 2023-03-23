14a0226d28f015df9a29a90d61968221b1336e54
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-03-23 14:02:50 +0000

    [add]tesuto

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,13 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment_method: {credit_card:0, transfer:1}
-  
+
   enum status: {wait_payment:0, confirm_payment:1, making:2, send_prepare:3, send_complete:4}
 
   def subtotal
     item.price * 1.10 * cart_item.amount
   end
-  
+
   def order_item_amount
     amount = 0
     order_details.each do |order_detail|
