5cd6b50bac02437054d7ebb5c3f426eebb783693
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-03-27 10:46:16 +0000

    [finish]tesuto

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,15 +1,15 @@
 class Admin::OrderDetailsController < ApplicationController
   def production
     order_detail = OrderDetail.find(params[:id])
+    order = order_detail.order
+    order_details = order.order_details
     order_detail.update(order_detail_params)
     if order_detail.making_status == "making"
       order_detail.order.update(status: "making")
-    elsif order_detail.making_status == "complete"
+    elsif order_details.complete.count == order_details.count
       order_detail.order.update(status: "send_prepare")
-    else
-      redirect_to admin_order_path(order_detail.order.id)
     end
-    redirect_to admin_order_path(order_detail.order.id)
+    redirect_to admin_order_path(order.id)
   end
 
   private
